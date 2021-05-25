9d47f7eb12b96fb07f2f4f1ec6a8bbe4c48ef052
hunter.lippmann.com <hunter.lippmann.com>
2021-05-24 22:56:48 -0400

    first commit

---
+++ sweg.py
@@ -0,0 +1,17 @@
+import os
+from datetime import datetime
+from selenium import webdriver
+from PIL import Image
+from io import StringIO
+
+url = 'https://forecast.predictwind.com/tracking/display/SV_Freya'
+
+os.environ['PATH'] = os.getcwd() + ':' + os.environ['PATH']
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--window-size=1920x1080")
+driver = webdriver.Chrome(chrome_options=chrome_options)
+driver.get(url)
+now = datetime.utcnow()
+driver.get_screenshot_as_file('screenshots/{}.png'.format(now))
